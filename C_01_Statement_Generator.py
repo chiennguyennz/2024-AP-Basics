e875e68e18cd28265450b8ada6103e936ea41902
chiennguyennz <159498633+chiennguyennz@users.noreply.github.com>
2024-02-23 13:00:17 +1300

    Add files via upload

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+#function go here
+# 1 usage
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+# Mainn routine goes here
+statement_generator("Instrucitons", "-")
