85266b73fe9c6bf71173b633d21d2d865f8d658e
chiennguyennz <159498633+chiennguyennz@users.noreply.github.com>
2024-02-24 22:03:17 +1300

    Factor Program

--- B_01_factors.py
+++ B_01_factors.py
@@ -42,6 +42,24 @@ def num_check(question):
             print(error)
 
 
+
+def factor(var_to_factor):
+    all_factors = []
+
+    for item in range(1, (to_factor+1)): #range is from 1 to to_factor
+        factor_pair = to_factor // item #get the number that pairs with a factor (item)
+        is_factor = to_factor % item #get remainder
+
+        if is_factor == 0: #if no remainders, then is factor
+            if item not in all_factors: #checks if factor and its pair exist in the list already
+                all_factors.append(item)
+            if factor_pair not in all_factors:
+                all_factors.append(factor_pair)
+            all_factors.sort()  # sort from smallest to biggest
+                #print(f"{item} and {factor_pair} are factors of {to_factor}!")
+
+    return all_factors
+
 # Main Routine Goes Here
 
 
@@ -55,8 +73,43 @@ if want_instructions == "":
     instructions()
 
 while True:
-    to_factor = num_check("To factor: ")
-    print("You chose to factor", to_factor)
+
+    comment = ""
+
+    # ask user for number to be factorised
+    to_factor = num_check("\nEnter an integer (or xxx to quit): ")
 
     if to_factor == "xxx":
         break
+
+    # get factors for integers that are 2 or more
+    elif to_factor != 1:
+        all_factors = factor(to_factor)
+    
+    # Set up comment for unity
+    else:
+        all_factors = ""
+        comment = "One is UNITY! It only has one factor. Itself :)"
+    
+    # comments for squares /  primes
+    
+    # Prime numbers have only two factors
+    if len(all_factors) == 2:
+        comment = f"{to_factor} is a prime number"
+    
+    # check if the list has an odd nunber of factors
+    elif len(all_factors) % 2 == 1:
+        comment = f"{to_factor} is a perfect square"
+
+    # Set up headings
+    if to_factor > 1:
+        heading = f"Factors of {to_factor}"
+    else:
+        heading = "One is special..."
+    # output factors and comment
+    print()
+    statement_generator(heading, "*")
+    print(all_factors)
+    print(comment)
+
+print("Thank you for using the factors calculator")
